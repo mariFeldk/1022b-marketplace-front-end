173dba19d2ae2d135fd4ecf2e40956f85da56833
mariFeldk <118355692+mariFeldk@users.noreply.github.com>
2024-10-22 07:33:03 -0400

    trabalho

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   },[])
 
   useEffect(()=>{
-    fetch("https://one022b-marketplace-cegf.onrender.com/usuario")
+    fetch("https://one022b-marketplace-cegf.onrender.com/usuarios")
     .then(resposta=>resposta.json())
     .then(dados=>setUsuario(dados))
   },[])
@@ -38,17 +38,17 @@ function App() {
       return(
         <div  key={prod.id} className='produto-item'>
           <h1>{prod.nome}</h1>
-          <p>{prod.imagem}</p>
+          <img src='{prod.imagem}'/>
           <p>{prod.preco}</p>
           <p>{prod.descricao}</p>
         </div>
       )
     })}
     </div>
-     <div className="container-usuario">
+     <div className="container-usuarios">
      {usuarios.map(usu=>{
       return(
-        <div  key={usu.id} className='usuario-item'>
+        <div  key={usu.id} className='usuarios-item'>
           <h1>{usu.nome}</h1>
           <p>{usu.email}</p>
           <p>{usu.created_at}</p>
